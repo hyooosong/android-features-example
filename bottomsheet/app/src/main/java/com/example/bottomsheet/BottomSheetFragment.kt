b49bc95f23d04759c05f8ba9c625e6bb2c8632d1
hyooosong <gythd1998@gmail.com>
2021-11-15 00:57:07 +0900

    [feature] Base BottomSheet

---
+++ bottomsheet/app/src/main/java/com/example/bottomsheet/BottomSheetFragment.kt
@@ -0,0 +1,29 @@
+package com.example.bottomsheet
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.bottomsheet.databinding.FragmentBottomSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetFragment : BottomSheetDialogFragment() {
+    private lateinit var binding: FragmentBottomSheetBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnClose.setOnClickListener {
+            dialog?.dismiss()
+        }
+    }
+}
--- bottomsheet/app/src/main/java/com/example/bottomsheet/MainActivity.kt
+++ bottomsheet/app/src/main/java/com/example/bottomsheet/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.bottomsheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bottomsheet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.activity = this
+        binding.lifecycleOwner = this
+    }
+
+    fun openDialog() {
+        val bottomSheetDialogFragment = BottomSheetFragment()
+        bottomSheetDialogFragment.show(supportFragmentManager, "TAG")
     }
 }
